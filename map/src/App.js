e450208202b155af1f5e95b6a4b4a147e9989502
조민수 <chominsu10@gmail.com>
2021-09-10 19:05:23 +0900

    [ADD] add map

--- map/src/App.js
+++ map/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div className="App">
       <div>app</div>
+      <Map />
     </div>
   );
 }
---
+++ map/src/components/Map.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const { kakao } = window;
+
+const Map = () => {
+  useEffect(() => {
+    const container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+  return (
+    <div>
+      <div
+        id="map"
+        style={{
+          width: "500px",
+          height: "500px",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default Map;
